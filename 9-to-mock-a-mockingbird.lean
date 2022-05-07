85ceeefd157b4b2db74edb1c12c6bd5e4394b46e
Guilherme Kunigami <kunigami@fb.com>
2022-05-07 13:33:26 -0700

    add solution for first 3 problems, ch 9

---
+++ 9-to-mock-a-mockingbird.lean
@@ -0,0 +1,108 @@
+import tactic
+
+inductive Bird
+| Call : Bird -> Bird -> Bird
+open Bird
+
+reserve infix ` ⬝ ` : 99
+
+universes u
+class has_call (α : Type u) := (call : α → α → α)
+infix ⬝ := has_call.call
+
+instance : has_call Bird := ⟨Call⟩
+
+/-
+ Problem 1: The significance of the Mockingbird
+-/
+
+def composes(a b c: Bird): Prop := ∀ x, c ⬝ x = a ⬝ (b ⬝ x)
+def is_mocking(m: Bird): Prop := ∀ x, m ⬝ x = x ⬝ x
+def is_fond(a b: Bird): Prop := a ⬝ b = b
+
+theorem fondness
+
+  -- Composition condition
+  (C₁: ∀ a b: Bird, ∃ c: Bird, composes a b c)
+  -- A mocking bird exists
+  (C₂: ∃ m: Bird, is_mocking m)
+
+  : ∀ a: Bird, ∃ b, is_fond a b :=
+begin
+  intro a,
+  cases C₂ with m Hm,
+  cases (C₁ a m) with c Hc,
+  -- CC:  c c = a (m c)
+  have CC := Hc c,
+  -- bind x with c
+  -- Hmc: m c = c c
+  have Hmc := Hm c,
+  -- replace c c = a (m c) with
+  -- c c = a (c c)
+  rw Hmc at CC,
+  -- bind x in the goal with c c
+  -- ⊢ is_fond a (c ⬝ c)
+  existsi c ⬝ c,
+  -- ⊢  = c ⬝ c
+  rw is_fond,
+  -- ⊢ c ⬝ c = a ⬝ (c ⬝ c)
+  symmetry,
+  exact CC,
+end
+
+/-
+ Problem 2: Egocentric?
+-/
+
+def is_egocentric(a: Bird): Prop := is_fond a a
+
+theorem egocentric_exists
+
+  -- Composition condition
+  (C₁: ∀ a b: Bird, ∃ c: Bird, composes a b c)
+  -- A mocking bird exists
+  (C₂: ∃ m: Bird, is_mocking m)
+
+  : ∃ x, is_egocentric x :=
+begin
+  cases C₂ with m Hm,
+  cases (C₁ m m) with c Hc,
+  have Hcc := Hc c,
+  repeat {rw Hm at Hcc},
+  existsi c ⬝ c,
+  rw is_egocentric,
+  rw is_fond,
+  symmetry,
+  exact Hcc,
+end
+
+/-
+ Problem 3: Story of the Agrreable Bird
+-/
+
+def is_agreeable(a: Bird): Prop := ∀ b: Bird, ∃ x: Bird, a ⬝ x = b ⬝ x
+
+theorem fondness_with_aggreable
+
+  -- Composition condition
+  (C₁: ∀ a b: Bird, ∃ c: Bird, composes a b c)
+  -- A aggreable bird exists
+  (C₂: ∃ a: Bird, is_agreeable a)
+
+  : ∀ a: Bird, ∃ b, is_fond a b :=
+
+begin
+  intro a,
+  cases C₂ with g Hg,
+  rw is_agreeable at Hg,
+  cases (C₁ a g) with c Hc,
+  have Hgc := Hg c,
+  cases Hgc with x₁ Hgcx,
+  have Hcx := Hc x₁,
+  rw Hgcx at Hcx,
+
+  existsi c ⬝ x₁,
+  rw is_fond,
+  symmetry,
+  exact Hcx,
+end
